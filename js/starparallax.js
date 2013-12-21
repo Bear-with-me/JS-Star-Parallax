bd735a22c6058ef73ca8a3bbf6294baf9f9583c6
Ikradex <ikradex@gmail.com>
2013-12-21 16:45:03 +0000

    Tweaks and performance fixes

--- js/starparallax.js
+++ js/starparallax.js
@@ -1,74 +1,96 @@
-var canvas = null,
-    container = document.getElementById("gameCanvasContainer");
+var canvas = document.createElement( "canvas" ),
+    ctx = canvas.getContext( "2d" ),
+    container = document.getElementById( "gameCanvasContainer" );
 
-canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-
-var DEFAULT_WIDTH = 800;
-var DEFAULT_HEIGHT = 600;
+var DEFAULT_WIDTH = 800, DEFAULT_HEIGHT = 600;
 
 canvas.width = DEFAULT_WIDTH;
 canvas.height = DEFAULT_HEIGHT;
 container.appendChild(canvas);
 
-var demo = new Demo();
-
-/*==============================*/
-/* Main Loop
-/*==============================*/
-
-var main = function () {
-    var now = Date.now(),
-        delta = now - then;
-
-    if (demo) {
-        demo.update(delta / 1000);
-        demo.render();
-    }
-
-    then = now;
-};
-
-var then = Date.now();
-setInterval(main, 1000 / demo.FPS);
-
-/*================================================*/
-/* Main Game Class */
-/*================================================*/
+var demo = new Demo( );
 
 /*
  * helper function
  * returns random integer between min and max
  */
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+var getRandomInt = function( min, max ) {
+    return Math.floor( Math.random( ) * ( max - min + 1 )) + min;
 }
 
-function Demo() {
-    this.FPS = 60;
+/*==============================*/
+/* Main Loop
+/*==============================*/
+
+var animationFrame = 
+    window.requestAnimationFrame       ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame    ||
+    window.oRequestAnimationFrame      ||
+    window.msRequestAnimationFrame     || null;
+
+var main = function( ) {
+    var now = Date.now( ),
+        delta = now - then;
+
+    if( demo ) {
+        demo.update( delta / 1000 );
+        demo.render( );
+    }
+
+    then = now;
+};
+
+var then = Date.now( );
+
+if( animationFrame !== null ) {
+    var mainLoop = function( ) {
+        main( );
+        animationFrame( mainLoop, canvas );
+    }
+
+    animationFrame( mainLoop, canvas );
+} else {
+    // for IE9
+    setInterval( main, 1000 / demo.FPS );
+}
+
+/*================================================*/
+/* Main Demo Class */
+/*================================================*/
+
+function Demo( ) {
+    this.FPS = 60; // for setInterval fallback only
     this.started = false;
-    this.bgLayers = new Array();
+    this.bgLayers = [];
     this.numBgLayers = 12;
-    this.baseStarNum = 6;
+    this.baseStarNum = 16;
+    this.baseStarRadius = 1.5;
+    this.baseStarSpeed = {
+        x: 0,
+        y: 100
+    };
 }
 
-Demo.prototype.update = function (delta) {
-    if (!this.started) {
-        this.started = true;
-        this.initBackground();
-        console.log("started");
-    } else {
-        for (var i = 0; i < this.bgLayers.length; i++) {
-            for (var j = 0; j < this.bgLayers[i].length; j++) {
-                var star = this.bgLayers[i][j];
+Demo.prototype.update = function(delta) {
+    if ( ! this.started ) {
+        // initialise demo
 
-                if (this.inBounds(star.x, star.y)) {
+        this.started = true;
+        this.initBackground( );
+        console.log( "Demo started" );
+    } else {
+        for( var i = 0; i < this.bgLayers.length; i++ ) {
+            for( var j = 0; j < this.bgLayers[ i ].length; j++ ) {
+                var star = this.bgLayers[ i ][ j ];
+
+                if( this.inBounds( star.x, star.y )) {
                     // move star
-                    star.x += star.speed.x;
-                    star.y += star.speed.y;
+                    star.x += star.speed.x * delta;
+                    star.y += star.speed.y * delta;
                 } else {
                     // reset star
-                    star.x = getRandomInt(0, canvas.width);
+                    star.x = getRandomInt( 0, canvas.width );
                     star.y = 1;
                 }
             }
@@ -76,26 +98,29 @@ Demo.prototype.update = function (delta) {
     }
 };
 
-Demo.prototype.render = function () {
-    ctx.fillStyle = "#000000";
-    ctx.strokeStyle = "#FFFFFF";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+Demo.prototype.render = function( ) {
+    ctx.fillStyle = "#000";
+    ctx.strokeStyle = "#FFF";
+    ctx.fillRect( 0, 0, canvas.width, canvas.height );
+    ctx.strokeRect( 0, 0, canvas.width, canvas.height );
+    ctx.fillStyle = "#FFF";
 
-    ctx.save(); // save context
-    ctx.fillStyle = ctx.strokeStyle = "#FFFFFF";
+    for ( var i = 0; i < this.bgLayers.length; i++) {
+        for( var j = 0; j < this.bgLayers[ i ].length; j++ ) {
+            var star = this.bgLayers[ i ][ j ];
 
-    for (var i = 0; i < this.bgLayers.length; i++) {
-        for (var j = 0; j < this.bgLayers[i].length; j++) {
-            var star = this.bgLayers[i][j];
+            if( star.radius >= 0.5 ) {
+                ctx.beginPath( );
+                ctx.arc( star.x, star.y, star.radius, 0, 2 * Math.PI, false );
+                ctx.fill( );
+            } else {
+                // circles cannot be seen below this point
+                // cheaper to render square pixels instead
 
-            ctx.beginPath();
-            ctx.arc(star.x, star.y, star.radius, 0, 2 * Math.PI, false);
-            ctx.fill();
-            ctx.stroke();
+                ctx.fillRect( star.x, star.y, 1, 1 );
+            }
         }
     }
-    ctx.restore(); // restore context
 };
 
 /*
@@ -104,26 +129,26 @@ Demo.prototype.render = function () {
  * layers have different speeds
  * stars in faster layers are larger
  */
-Demo.prototype.initBackground = function () {
-    for (var i = this.numBgLayers; i > 0; i--) {
-        var stars = new Array();
+Demo.prototype.initBackground = function( ) {
+    for( var i = this.numBgLayers; i > 0; i-- ) {
+        var stars = [];
 
-        for (var j = 0; j < this.baseStarNum * i; j++) {
+        for( var j = 0; j < this.baseStarNum * i; j++ ) {
             var star = {
-                x: getRandomInt(0, canvas.width),
-                y: getRandomInt(0, canvas.height),
-                radius: 1 / i,
+                x: getRandomInt( 0, canvas.width ),
+                y: getRandomInt( 0, canvas.height ),
+                radius: Math.round( this.baseStarRadius / i * 100 ) / 100,
                 speed: {
-                    x: 0,
-                    y: 2 / i
+                    x: Math.round( this.baseStarSpeed.x / i * 100 ) / 100,
+                    y: Math.round( this.baseStarSpeed.y / i * 100 ) / 100 
                 }
             };
-            stars.push(star);
+            stars.push( star );
         }
-        this.bgLayers.push(stars);
+        this.bgLayers.push( stars );
     }
 };
 
-Demo.prototype.inBounds = function (x, y) {
-    return ((x > 0 && x < canvas.width) && (y > 0 && y < canvas.height));
+Demo.prototype.inBounds = function( x, y ) {
+    return (( x > 0 && x < canvas.width ) && ( y > 0 && y < canvas.height ));
 };
